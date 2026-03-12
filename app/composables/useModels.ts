d27f994a94509aaaa7d68dd295ebb504630225c0
Jing Rong Yu <60832603+yujingr@users.noreply.github.com>
2026-03-12 18:01:28 +0800

    s

--- app/composables/useModels.ts
+++ app/composables/useModels.ts
@@ -1,7 +1,7 @@
 import { MODELS } from '#shared/utils/models'
 
 export function useModels() {
-  const model = useCookie<string>('model', { default: () => 'openai/gpt-5-nano' })
+  const model = useCookie<string>('model', { default: () => 'anthropic/claude-opus-4.6' })
 
   return {
     models: MODELS,
--- shared/utils/models.ts
+++ shared/utils/models.ts
@@ -1,8 +1,8 @@
 export const MODELS = [
-  { label: 'Claude Opus 4.6', value: 'anthropic/claude-opus-4.6', icon: 'i-simple-icons-anthropic' },
-  { label: 'GPT-5.4', value: 'openai/gpt-5.4', icon: 'i-simple-icons-openai'},
-  { label: 'GPT-5 Nano', value: 'openai/gpt-5-nano', icon: 'i-simple-icons-openai' },
-  { label: 'Gemini 3.1 Flash Image Preview', value: 'google/gemini-3.1-flash-image-preview', icon: 'i-simple-icons-google' },
-  { label: 'Gemini 3.1 Flash Lite', value: 'google/gemini-3.1-flash-lite-preview', icon: 'i-simple-icons-google' },
-  { label: 'Gemini 3.1 Pro Preview', value: 'google/gemini-3.1-pro-preview', icon: 'i-simple-icons-google'},
+  { label: '最智能Claude Opus 4.6', value: 'anthropic/claude-opus-4.6', icon: 'i-simple-icons-anthropic' },
+  { label: '最智能GPT-5.4', value: 'openai/gpt-5.4', icon: 'i-simple-icons-openai'},
+  { label: '小快GPT-5 Nano', value: 'openai/gpt-5-nano', icon: 'i-simple-icons-openai' },
+  { label: '可以生图Gemini 3.1 Flash Image Preview', value: 'google/gemini-3.1-flash-image-preview', icon: 'i-simple-icons-google' },
+  { label: '快Gemini 3.1 Flash Lite', value: 'google/gemini-3.1-flash-lite-preview', icon: 'i-simple-icons-google' },
+  { label: '最智能Gemini 3.1 Pro Preview', value: 'google/gemini-3.1-pro-preview', icon: 'i-simple-icons-google'},
 ]
